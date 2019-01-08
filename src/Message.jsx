c0753f82a9b18587f0e13d06b8439aa684266392
Zixia Lu <zixialu@gmail.com>
2019-01-08 11:33:32 -0500

    Add more components

---
+++ src/Message.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class Message extends Component {
+  render() {
+    return (
+      <div className="message">
+        <span className="message-username">{this.props.message.username}</span>
+        <span className="message-content">{this.props.message.content}</span>
+      </div>
+    );
+  }
+}
---
+++ src/MessageList.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+import Message from './Message.jsx';
+
+export default class MessageList extends Component {
+  render() {
+    return this.props.messages.map(message => <Message message={message} />);
+  }
+}
